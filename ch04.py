e98b70f6acefe0deecdf873cb6aee1611253f8a3
OOO-ly <lh190217@gmail.com>
2020-11-12 16:57:17 +0900

    if문 1-1

--- ch04.py
+++ ch04.py
@@ -9,4 +9,58 @@ print(type(False)) # 0,"". [], (), {}
 #에1
 
 if 'a':
+    print('Bad')
+else:
     print('Good')
+
+
+#Ex 2
+if False:
+    print('Bad!')
+else:
+    print('Good')
+
+#Ex3
+#관게 연산자
+# > , >= , < , <= , == < !=
+
+x = 15
+y = 10
+
+#양변이 같을 경우 참
+print(x == y)
+
+#양번이 다를 때 참
+print(x != y)
+
+#왼쪽이 클 때 참
+print(x > y)
+#왼쪽이 크거나 같을 때 참
+print(x >= y)
+#오른쪽이 클 때 참
+print(x < y)
+#오른쪽이 크거나 같을 때 참
+print(x <= y)
+
+
+city = ''
+
+if city:
+    print(city)
+else:
+    print('city is empty')
+
+city2 = 'jeju'
+
+if city2:
+    print('asdasd',city2)
+else:
+    print('city is : ',city2)
+
+
+
+
+
+
+
+#
